28bedd71ba5cc7494205270508b28a5e1a3f39e0
IsakLerum <isak.lerum@gmail.com>
2022-06-02 14:26:45 +0200

    added chuncks"packages", "sleepstudy" & "modify data and plot" from Datalab09 in BIO300B

--- test.R
+++ test.R
@@ -1,1 +1,27 @@
 runif(1)
+
+# ```{r packages, echo=FALSE}
+library(tidyverse)
+library(lme4)
+library(ggbeeswarm)
+library(broom.mixed)
+library(lattice)
+# ```
+
+# ```{r sleepstudy}
+data("sleepstudy", package = "lme4")
+sleepstudy
+# ```
+
+# ```{r modify data and plot}
+corrected_sleepstudy <- sleepstudy |> 
+  filter(Days > 1)
+
+reaction_plot <- ggplot(corrected_sleepstudy, aes(Days, Reaction, group = Days, color = Subject)) +
+  geom_violin(show.legend = FALSE) +
+  geom_quasirandom(show.legend = FALSE) +
+  scale_x_continuous(n.breaks = 8) +
+  labs(y = "Reaction time (ms)", x = "Days of sleep deprivation")
+
+reaction_plot
+# ```
